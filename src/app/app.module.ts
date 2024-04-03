c3425ec4aa446c8ed30e536f1f6ce89a6b78d6dc
Divyaraj Makwana <divyaraj.makwana9425@gmail.com>
2024-04-03 10:01:07 +0530

    created first homemade directive of basicHighlight

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { basicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
+    basicHighlightDirective
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
